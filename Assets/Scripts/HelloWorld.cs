4d14f7a5c16ffbc529eb1190a9949d4b069d3326
hrum <qubefuuu@mail.ru>
2023-10-26 20:31:38 +0500

    Create Repository

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public void buttonClick()
+    {
+        Debug.Log("Hello World");
+    }
+}
